ca8d2b03866a9fad040dc16ceb41d98d6af0dbcf
Gerard64990 <gerardpasmajax@gmail.com>
2020-10-24 11:42:49 +0200

    Update regaz.sh

--- regaz.sh
+++ regaz.sh
@@ -9,7 +9,7 @@ then
     datefile="gaz_"$month$year
 fi
 echo "Curl"
-curl --silent -X POST "https://www.myregaz.com/api-public/consumption/183109/statistics/xls" --data "month="$month"&year="$year --output $datefile".xlsx" > /dev/null
+curl --silent -X POST "https://www.myregaz.com/api-public/consumption/ID_TO_REPLACE/statistics/xls" --data "month="$month"&year="$year --output $datefile".xlsx" > /dev/null
 
 echo "Call xlsx2csv.py"
 /xlsx2csv.py $datefile
